9ea55a19d42d922bc6bda1306efac6b36254e84f
Narendra Samtani <alenway97@gmail.com>
2025-05-05 19:27:12 +0530

    My Lazy Vim setup...

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,5 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- Map 'jk' to escape from Insert mode to Normal mode:
