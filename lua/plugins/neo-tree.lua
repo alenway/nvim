ea3490e933fc1499950958abb1b69504985bdc95
Narendra Samtani <alenway97@gmail.com>
2025-10-22 12:20:15 +0530

    right align the neo tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,9 @@
+return {
+      "nvim-neo-tree/neo-tree.nvim",
+      opts = {
+            window = {
+                  position = "right", -- move Neo-tree to the right
+                  width = 30, -- optional
+            },
+      },
+}
